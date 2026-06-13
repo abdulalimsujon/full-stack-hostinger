8be89713cc1b7c02125740ea3212c0f9c1c2f3ad
abdulalimsujon <alimsujon12@gmail.com>
2026-06-14 01:23:52 +0600

    enable cors

--- server/index.js
+++ server/index.js
@@ -10,6 +10,7 @@ app.use(
   cors({
     origin: "http://localhost:5173", // React/Vite frontend
     origin: "http://localhost:5174",
+    origin: "http://172.237.65.209:5174",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   }),
